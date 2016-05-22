645c326f1b5ee35b6aeea8a3b8bcabb04e58c6e4
RoronoaZoroZrh <137147783@qq.com>
2016-05-22 22:12:19 +0800

    新项目Plan
    记录计划

---
+++ CSharpLearning/Plan/Form/Plan.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Plan
+{
+    partial class Plan
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Plan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 486);
+            this.Name = "Plan";
+            this.Text = "计划";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
