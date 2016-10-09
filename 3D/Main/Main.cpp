ee1c3970ff48c8488a2990c6950e9f5c80900533
RoronoaZoroZrh <137147783@qq.com>
2016-10-09 11:01:57 +0800

    3D
    Main

---
+++ 3D/Main/Main.cpp
@@ -0,0 +1,13 @@
+/********************************************************************
+@file     Main.cpp
+@date     2016-10-9
+@author   Zoro_Tiger
+@brief    Windows程序的最小需求
+********************************************************************/
+#include <cstdio>
+
+int main(int argc, const char* argv[])
+{
+	printf("There can be only one!\n");
+	return 0;
+}
