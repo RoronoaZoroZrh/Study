1243a91cd0499036cfe6b6d2fcbd95d80816a6c4
RoronoaZoroZrh <137147783@qq.com>
2017-03-10 18:03:00 +0800

    3D
    QianMo_7

---
+++ 3D/QianMo_7/Main.cpp
@@ -0,0 +1,19 @@
+/********************************************************************
+@file     Main.cpp
+@date     2017-3-10
+@author   Zoro_Tiger
+@brief    基础动画显示（一）定时器的使用
+********************************************************************/
+//!头文件
+#include <Windows.h>
+#include <tchar.h>
+
+//!程序入口
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nShowCmd)
+{
+	MSG msg;
+	return msg.wParam; //!返回退出消息
+}
