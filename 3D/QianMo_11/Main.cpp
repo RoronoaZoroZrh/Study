3d64050af891c51f0e16f7dc3e37436c60dfa969
RoronoaZoroZrh <137147783@qq.com>
2017-06-28 16:35:18 +0800

    3D
    QianMo_11

--- 3D/QianMo_11/Main.cpp
+++ 3D/QianMo_11/Main.cpp
@@ -5,9 +5,9 @@
 @brief    基础动画显示（四） 排序贴图
 ********************************************************************/
 //!头文件
-#include <Windows.h>
 #include <tchar.h>
 #include <cstring>
+#include "DataDefine.h"
 
 //!宏定义
 #define WINDOW_CLASS _T("Chapter 11 Class")  //!类名
@@ -21,6 +21,7 @@ HDC g_mdc = NULL;
 HDC g_bufferdc = NULL;
 HBITMAP g_bitmapArray[BITMAP_NUM] = { NULL };
 HBITMAP g_background = NULL;
+Dragon  g_dragon[DRAGON_NUM];
 
 //!函数声明
 void Render(UINT nTick);
@@ -104,6 +105,13 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	g_bitmapArray[3] = (HBITMAP)LoadImage(NULL, _T("dra3.bmp"), IMAGE_BITMAP, 760, 198, LR_LOADFROMFILE);
 	g_background = (HBITMAP)LoadImage(NULL, _T("bg.bmp"), IMAGE_BITMAP, 640, 480, LR_LOADFROMFILE);
 
+	for (int i = 0; i < DRAGON_NUM; ++i)
+	{
+		g_dragon[i].x = 200;
+		g_dragon[i].y = 200;
+		g_dragon[i].dir = 3;
+	}
+
 	//!获取消息
 	MSG msg;
 	GetMessage(&msg, NULL, 0, 0);
@@ -122,10 +130,31 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	return msg.wParam;
 }
 
+void BubSort(void)
+{
+	for (int i = 0; i < DRAGON_NUM - 1; ++i)
+	{
+		bool order = true;
+		for (int j = 0; j < DRAGON_NUM - i - 1; ++j)
+		{
+			if (g_dragon[j].y > g_dragon[j + 1].y)
+			{
+				auto temp = g_dragon[j];
+				g_dragon[j] = g_dragon[j + 1];
+				g_dragon[j + 1] = temp;
+				order = false;
+			}
+		}
+		
+		if (order) break;
+	}
+}
+
 //!渲染
 void Render(UINT nTick)
 {
 	static int nPreTime = 0; //!前次渲染时间
+	static int nPictNum = 0; //!当前恐龙下标
 
 	//!渲染
 	if (nTick - nPreTime >= RENDER_TIME)
@@ -135,6 +164,147 @@ void Render(UINT nTick)
 		//!渲染背景
 		SelectObject(g_bufferdc, g_background);
 		BitBlt(g_mdc, 0, 0, 640, 480, g_bufferdc, 0, 0, SRCCOPY);
+
+		//!排序
+		BubSort();
+
+		//!渲染恐龙
+		for (int i = 0; i < DRAGON_NUM; ++i)
+		{
+			SelectObject(g_bufferdc, g_bitmapArray[g_dragon[i].dir]);
+
+			int w, h;
+			switch (g_dragon[i].dir)
+			{
+			case 0:
+				{
+					w = 66;
+					h = 94;
+					break;
+				}
+			case 1:
+				{
+					w = 68;
+					h = 82;
+					break;
+				}
+			case 2:
+				{
+					w = 95;
+					h = 99;
+					break;
+				}
+			case 3:
+				{
+					w = 95;
+					h = 99;
+					break;
+				}
+			}
+
+			BitBlt(g_mdc, g_dragon[i].x, g_dragon[i].y, w, h, g_bufferdc, nPictNum * w, h, SRCAND);
+			BitBlt(g_mdc, g_dragon[i].x, g_dragon[i].y, w, h, g_bufferdc, nPictNum * w, 0, SRCPAINT);
+		}
+
 		BitBlt(g_hdc, 0, 0, 640, 480, g_mdc, 0, 0, SRCCOPY);
+
+		for(int i = 0; i < DRAGON_NUM; ++i)  
+		{  
+			switch(rand() % 4)
+			{  
+			case 0:
+				switch(g_dragon[i].dir)  
+				{  
+				case 0:   
+					g_dragon[i].y -= 20;  
+					break;  
+				case 1:  
+					g_dragon[i].x += 2;  
+					g_dragon[i].y -= 31;  
+					break;  
+				case 2:   
+					g_dragon[i].x += 14;  
+					g_dragon[i].y -= 20;  
+					break;  
+				case 3:  
+					g_dragon[i].x += 14;  
+					g_dragon[i].y -= 20;  
+					break;  
+				}  
+
+				if(g_dragon[i].y < 0)  
+					g_dragon[i].y = 0;  
+				g_dragon[i].dir = 0;  
+				break;
+			case 1:
+				switch(g_dragon[i].dir)  
+				{  
+				case 0:  
+					g_dragon[i].x -= 2;  
+					g_dragon[i].y += 31;  
+					break;  
+				case 1:  
+					g_dragon[i].y += 20;  
+					break;  
+				case 2:  
+					g_dragon[i].x += 15;  
+					g_dragon[i].y += 29;  
+					break;  
+				case 3:  
+					g_dragon[i].x += 15;  
+					g_dragon[i].y += 29;  
+					break;  
+				}  
+
+				if(g_dragon[i].y > 370)  
+					g_dragon[i].y = 370;  
+				g_dragon[i].dir = 1;  
+				break;  
+			case 2:
+				switch(g_dragon[i].dir)  
+				{  
+				case 0:  
+					g_dragon[i].x -= 34;  
+					break;  
+				case 1:  
+					g_dragon[i].x -= 34;  
+					g_dragon[i].y -= 9;  
+					break;  
+				case 2:  
+					g_dragon[i].x -= 20;  
+					break;  
+				case 3:  
+					g_dragon[i].x -= 20;  
+					break;  
+				}  
+				if(g_dragon[i].x < 0)  
+					g_dragon[i].x = 0;  
+				g_dragon[i].dir = 2;  
+				break;  
+			case 3:
+				switch(g_dragon[i].dir)  
+				{  
+				case 0:  
+					g_dragon[i].x += 6;  
+					break;  
+				case 1:  
+					g_dragon[i].x += 6;  
+					g_dragon[i].y -= 10;  
+					break;  
+				case 2:  
+					g_dragon[i].x += 20;  
+					break;  
+				case 3:  
+					g_dragon[i].x += 20;  
+					break;  
+				}  
+				if(g_dragon[i].x > 535)  
+					g_dragon[i].x = 535;  
+				g_dragon[i].dir = 3;  
+				break;  
+			}  
+		}  
+
+		++nPictNum; nPictNum %= 8;
 	}
 }
