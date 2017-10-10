c6dea50fa73fbe1ca7c3ccb30bbc16e11ee3e3a7
RoronoaZoroZrh <137147783@qq.com>
2017-10-10 11:58:27 +0800

    修改工程配置

--- CSharpLearning/AdvTree_Operation_20171010_1/Form1.cs
+++ CSharpLearning/AdvTree_Operation_20171010_1/Form1.cs
@@ -1,7 +1,7 @@
 ﻿/**
  *  author : Tiger
  *  date   : 2017-10-10 11:33:40
- *  brief  : 为了验证AdvTree控件是否可以定位滚动条的位置
+ *  brief  : 为了验证AdvTree控件是否可以定位滚动条的位置（刷新后不可定位）
  */
 using System;
 using System.Windows.Forms;
@@ -19,19 +19,26 @@ namespace AdvTree_Operation_20171010_1
         //!构造AdvTree控件
         private void AdvTree_OP_Load(object sender, EventArgs e)
         {
+            RefreshAdvTree();
+        }
+
+        //!刷新
+        private void RefreshAdvTree()
+        {
             AdvTree_Item.Nodes.Clear();
             for (int i = 0; i < 100; i++)
             {
                 AdvTree_Item.Nodes.Add(new DevComponents.AdvTree.Node()
-                    {
-                        Text = i.ToString()
-                    });
+                {
+                    Text = i.ToString()
+                });
             }
         }
 
         //!定位滚动条位置
         private void AdvTree_Item_Click(object sender, EventArgs e)
         {
+            RefreshAdvTree();
             DevComponents.AdvTree.Node vSelNode = AdvTree_Item.Nodes[AdvTree_Item.Nodes.Count - 1];
             AdvTree_Item.SelectNode(vSelNode, DevComponents.AdvTree.eTreeAction.Code);
             vSelNode.EnsureVisible();
