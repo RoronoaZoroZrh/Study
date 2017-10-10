31cbf51605647c443bceaf0b16f828a0334cbdb3
RoronoaZoroZrh <137147783@qq.com>
2017-10-10 11:36:31 +0800

    CSharpLearning
    AdvTree_Operation_20171010_1

---
+++ CSharpLearning/AdvTree_Operation_20171010_1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace AdvTree_Operation_20171010_1
+{
+    partial class AdvTree_OP
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AdvTree_Item = new DevComponents.AdvTree.AdvTree();
+            this.nodeConnector1 = new DevComponents.AdvTree.NodeConnector();
+            this.elementStyle1 = new DevComponents.DotNetBar.ElementStyle();
+            ((System.ComponentModel.ISupportInitialize)(this.AdvTree_Item)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AdvTree_Item
+            // 
+            this.AdvTree_Item.AccessibleRole = System.Windows.Forms.AccessibleRole.Outline;
+            this.AdvTree_Item.AllowDrop = true;
+            this.AdvTree_Item.BackColor = System.Drawing.SystemColors.Window;
+            // 
+            // 
+            // 
+            this.AdvTree_Item.BackgroundStyle.Class = "TreeBorderKey";
+            this.AdvTree_Item.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AdvTree_Item.Location = new System.Drawing.Point(0, 0);
+            this.AdvTree_Item.Name = "AdvTree_Item";
+            this.AdvTree_Item.NodesConnector = this.nodeConnector1;
+            this.AdvTree_Item.NodeStyle = this.elementStyle1;
+            this.AdvTree_Item.PathSeparator = ";";
+            this.AdvTree_Item.Size = new System.Drawing.Size(440, 384);
+            this.AdvTree_Item.Styles.Add(this.elementStyle1);
+            this.AdvTree_Item.TabIndex = 0;
+            this.AdvTree_Item.Click += new System.EventHandler(this.AdvTree_Item_Click);
+            // 
+            // nodeConnector1
+            // 
+            this.nodeConnector1.LineColor = System.Drawing.SystemColors.ControlText;
+            // 
+            // elementStyle1
+            // 
+            this.elementStyle1.Class = "";
+            this.elementStyle1.Name = "elementStyle1";
+            this.elementStyle1.TextColor = System.Drawing.SystemColors.ControlText;
+            // 
+            // AdvTree_OP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 384);
+            this.Controls.Add(this.AdvTree_Item);
+            this.Name = "AdvTree_OP";
+            this.Text = "AdvTree操作练习";
+            this.Load += new System.EventHandler(this.AdvTree_OP_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.AdvTree_Item)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.AdvTree.AdvTree AdvTree_Item;
+        private DevComponents.AdvTree.NodeConnector nodeConnector1;
+        private DevComponents.DotNetBar.ElementStyle elementStyle1;
+    }
+}
+
---
+++ CSharpLearning/AdvTree_Operation_20171010_1/Form1.cs
@@ -0,0 +1,40 @@
+﻿/**
+ *  author : Tiger
+ *  date   : 2017-10-10 11:33:40
+ *  brief  : 为了验证AdvTree控件是否可以定位滚动条的位置
+ */
+using System;
+using System.Windows.Forms;
+
+namespace AdvTree_Operation_20171010_1
+{
+    public partial class AdvTree_OP : Form
+    {
+        //!构造函数
+        public AdvTree_OP()
+        {
+            InitializeComponent();
+        }
+
+        //!构造AdvTree控件
+        private void AdvTree_OP_Load(object sender, EventArgs e)
+        {
+            AdvTree_Item.Nodes.Clear();
+            for (int i = 0; i < 100; i++)
+            {
+                AdvTree_Item.Nodes.Add(new DevComponents.AdvTree.Node()
+                    {
+                        Text = i.ToString()
+                    });
+            }
+        }
+
+        //!定位滚动条位置
+        private void AdvTree_Item_Click(object sender, EventArgs e)
+        {
+            DevComponents.AdvTree.Node vSelNode = AdvTree_Item.Nodes[AdvTree_Item.Nodes.Count - 1];
+            AdvTree_Item.SelectNode(vSelNode, DevComponents.AdvTree.eTreeAction.Code);
+            vSelNode.EnsureVisible();
+        }
+    }
+}
