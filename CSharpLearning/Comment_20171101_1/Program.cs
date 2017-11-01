5726309d0d04a7e7c20e4ce72a882c8dd015c500
RoronoaZoroZrh <137147783@qq.com>
2017-11-01 16:26:11 +0800

    Comment_20171101_1

---
+++ CSharpLearning/Comment_20171101_1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ *  author : Tiger
+ *  date   : 2017-11-1 16:22:41
+ *  brief  : 查看文档注释直接生成xml文件功能
+ */
+using System;
+
+namespace Comment_20171101_1
+{
+    /// <summary>
+    /// class Program
+    /// </summary>
+    public class Program
+    {
+        /**
+         * method Main
+         */
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
