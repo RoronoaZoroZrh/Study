1ab2c30fb3158e1aea842c3a5ff88c370303a06e
RoronoaZoroZrh <137147783@qq.com>
2017-12-25 10:03:15 +0800

    Cookie_20171225_1

---
+++ CSharpLearning/Cookie/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ *  author : Zoro
+ *  date   : 2017-12-25 09:55:17
+ *  brief  : 查看cookie
+ */
+using System;
+using System.Net;
+
+namespace Cookie_20171225_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //!请求
+            HttpWebRequest vRequest = (HttpWebRequest)WebRequest.Create("https://www.baidu.com/index.php?tn=monline_3_dg");
+            vRequest.CookieContainer = new CookieContainer();
+
+            //!响应
+            HttpWebResponse vResponse = (HttpWebResponse)vRequest.GetResponse();
+
+            //!cookie
+            for (int i = 0; i < vResponse.Cookies.Count; i++)
+            {
+                Console.WriteLine("Name = {0}, Value = {1}", vResponse.Cookies[i].Name, vResponse.Cookies[i].Value);
+            }
+            vResponse.Close();
+
+            //!示例结束
+            Console.WriteLine("Sample Done!");
+            Console.ReadLine();
+        }
+    }
+}
