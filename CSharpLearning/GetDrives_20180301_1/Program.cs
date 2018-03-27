8018059a38d83561f007849e432fd52287f988d9
RoronoaZoroZrh <137147783@qq.com>
2018-03-27 11:12:39 +0800

    HttpWebResponse.LastModified

---
+++ CSharpLearning/GetDrives_20180301_1/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ *  author : Zoro
+ *  date   : 2018-3-1 10:25:05
+ *  brief  : 查看Drive
+ */
+using System;
+using System.IO;
+
+namespace GetDrives_20180301_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] vDriveInfos = DriveInfo.GetDrives();
+            foreach (DriveInfo vDriveInfo in vDriveInfos)
+            {
+                Console.WriteLine(vDriveInfo.Name);
+                Console.WriteLine(vDriveInfo.AvailableFreeSpace);
+            }
+
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            foreach (DriveInfo vDriveInfo in vDriveInfos)
+            {
+                long nSize = 100 * 1024 * 1024 * 1024L;
+                if (Directory.GetCurrentDirectory().Contains(vDriveInfo.Name) &&
+                    vDriveInfo.AvailableFreeSpace < nSize)
+                {
+                    Console.WriteLine(vDriveInfo.AvailableFreeSpace);
+                    Console.WriteLine(nSize);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
